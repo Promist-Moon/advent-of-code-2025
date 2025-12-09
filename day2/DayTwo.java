65b05cbf814d1b370616b4c55cf7f770b3a8eb39
Emilia Ayu <emiliaayu944@gmail.com>
2025-12-09 15:37:33 +0800

    Add solution code for day 2 part 1 and 2

--- day2/DayTwo.java
+++ day2/DayTwo.java
@@ -6,12 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import day5.Range;
-
 public class DayTwo {
 
     public static void main(String[] args) throws IOException {
-        String filename = "day5/input.txt";
+        String filename = "day2/input.txt";
 
         List<Range> ranges = new ArrayList<>();
 
@@ -20,12 +18,18 @@ public class DayTwo {
 
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                ranges = parseRanges(values);
+                ranges.addAll(parseRanges(values));
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        long sum = countInvalids(ranges);
+        System.out.println("Total sum of invalids: " + sum);
+
+        long newSum = countNewInvalids(ranges);
+        System.out.println("Total sum of new invalids: " + newSum);
 
     }
 
@@ -44,5 +48,39 @@ public class DayTwo {
         }
 
         return ranges;
+    }
+
+    /**
+     * Counts the sum of invalid ids for part 1, where an id is invalid if
+     * a sequence of digits is repeated exactly twice.
+     *
+     * @param ranges
+     * @return
+     */
+    private static long countInvalids(List<Range> ranges) {
+        long sum = 0;
+        for (Range range : ranges) {
+            //System.out.println(range.findInvalidIds());
+            sum += range.findInvalidIdsSum();
+        }
+
+        return sum;
+    }
+
+    /**
+     * Counts the sum of invalid ids in a range for part 1, where an id is invalid if
+     * a sequence of digits is repeated at least twice.
+     *
+     * @param ranges
+     * @return
+     */
+    private static long countNewInvalids(List<Range> ranges) {
+        long sum = 0;
+        for (Range range : ranges) {
+            // System.out.println(range.findNewInvalidIds());
+            sum += range.findNewInvalidIdsSum();
+        }
+
+        return sum;
     }
 }
--- day2/Range.java
+++ day2/Range.java
@@ -9,9 +9,15 @@ public class Range {
     private long start;
     private long end;
 
+    private int startLength;
+    private int endLength;
+
     public Range(long start, long end) {
         this.start = start;
         this.end = end;
+
+        this.startLength = String.valueOf(start).length();
+        this.endLength = String.valueOf(end).length();
     }
 
     // Parses a string in format [start]-[end] into a Range object
@@ -39,28 +45,6 @@ public class Range {
         return new Range(start, end);
     }
 
-    public static List<Range> mergeRanges(List<Range> ranges) {
-        List<Range> nonOverlapRanges = new ArrayList<>();
-
-        ranges.sort(Comparator.comparingLong(Range::getStart));
-
-        Range current = ranges.get(0);
-
-        for (int i = 1; i < ranges.size(); i++) {
-            Range next = ranges.get(i);
-            if (current.isCombinable(next)) {
-                current = current.combine(next);
-                continue;
-            }
-            nonOverlapRanges.add(current);
-            current = next;
-        }
-
-        nonOverlapRanges.add(current);
-        return nonOverlapRanges;
-
-    }
-
     public long getStart() {
         return start;
     }
@@ -74,33 +58,89 @@ public class Range {
         return end - start + 1;
     }
 
-    // Add method for checking overlap
-    public boolean contains(long value) {
-        return value >= start && value <= end;
-    }
-
     /**
-     * Checks if two Ranges are combinable.
-     * Two ranges are combinable if the ranges overlap.
+     * Finds a list of invalid ids in the range, where an invalid id has a sequence of
+     * digits repeated exactly twice.
      *
-     * @param other
-     * @return true if combinable
-     */
-    public boolean isCombinable(Range other) {
-        return this.start <= other.end && other.start <= this.end;
-    }
-
-    /**
-     * Combines combinable ranges.
-     *
-     * @param other
      * @return
      */
-    public Range combine(Range other) {
-        long newStart = Math.min(this.getStart(), other.getStart());
-        long newEnd = Math.max(this.getEnd(), other.getEnd());
+    public List<Long> findInvalidIds() {
+        List<Long> ids = new ArrayList<>();
 
-        return new Range(newStart, newEnd);
+        for (int i = 2; i < startLength; i++) {
+            for (long current = start; current <= end; current++) {
+                String s = Long.toString(current);
+                int len = s.length();
+                if (len % i != 0) continue;
+
+                int half = len / 2;
+                String left = s.substring(0, half);
+                String right = s.substring(half);
+
+                if (left.equals(right)) {
+                    ids.add(current);
+                }
+            }
+        }
+
+        return ids;
+    }
+
+    /**
+     * Finds the sum of invalid ids.
+     *
+     * @return
+     */
+    public long findInvalidIdsSum() {
+        long sum = findInvalidIds().stream()
+                .reduce(0L, Long::sum);
+
+        return sum;
+    }
+
+    /**
+     * Finds a list of invalid ids in the range, where an invalid id has a sequence of
+     * digits repeated at least twice.
+     *
+     * @return
+     */
+    public List<Long> findNewInvalidIds() {
+        List<Long> ids = new ArrayList<>();
+
+        for (int i = 2; i <= endLength; i++) {
+            for (long current = start; current <= end; current++) {
+                String s = Long.toString(current);
+                int len = s.length();
+                if (len % i != 0) continue;
+
+                int sect = len / i;
+                List<String> sections = new ArrayList<>();
+                for (int j = 0; j < i; j++) {
+                    String section = s.substring(j * sect, (j + 1) * sect);
+                    sections.add(section);
+                }
+
+                boolean allEqual = sections.stream().distinct().limit(2).count() == 1;
+
+                if (allEqual && !ids.contains(current)) {
+                    ids.add(current);
+                }
+            }
+        }
+
+        return ids;
+    }
+
+    /**
+     * Finds the sum of new invalid ids.
+     *
+     * @return
+     */
+    public long findNewInvalidIdsSum() {
+        long sum = findNewInvalidIds().stream()
+                .reduce(0L, Long::sum);
+
+        return sum;
     }
 
     // Override equals() and hashCode() to compare
